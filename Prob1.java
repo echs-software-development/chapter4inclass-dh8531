e7054f6e64803eed4653e84ada3ca46f8075a6f5
DYLAN HANNIGAN <dh8531@students.necsd.net>
2020-12-28 22:51:03 +0000

    i did the problems.

--- Prob1.java
+++ Prob1.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 
 public class Prob1 {
   public static void main(String[] args){
+/* Write your code here */
+  Scanner scan = new Scanner(System.in);
+  
+  System.out.println("Enter a positive integer.");
+  int n = scan.nextInt();
 
+  double ran = Math.random();
+  int range = n;
+  int min = 1;
+  int answer = (int) (ran * range) + min;
+  
+  System.out.println("Printing 10 random integers from 1 to n...");
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  System.out.println(answer);
+  }
+}
   }
 }
--- Prob3.java
+++ Prob3.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 
 public class Prob3 {
   public static void main(String[] args) {
-    
+Scanner input = new Scanner(System.in);
 
+		// Prompt the user to enter an ASCII code
+		// (an integer between 0 and 127)
+		System.out.print("Enter an ASCII code: ");
+		int i = input.nextInt();
 
-  }
+		// Display ASCII code as character
+		System.out.println((char)i);
+	}
 }
